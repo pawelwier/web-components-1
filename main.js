f13c9d28fcf8a5df8cfdfdee432a7f6e643a30c2
Paweł Wierciński <47865579+pawelwier@users.noreply.github.com>
2020-12-29 12:45:36 +0100

    Simple components, button triggering event inside

---
+++ main.js
@@ -0,0 +1,51 @@
+const userTemplate = document.createElement('template');
+userTemplate.innerHTML = `
+    <div class="main">
+        <div class="user-name"></div>
+        <div class="user-age"></div>
+        <button class="age-button">Hide age</button>
+    </div>
+    <style>
+        .main {
+            display: grid;
+            grid-template-columns: 2fr 1fr 2fr;
+            width: 300px;
+            border-bottom: 2px black solid;
+            margin-bottom: 10px;
+            background-color: #e8e8e8;
+            padding:3px;
+        }
+    </style>
+`
+
+class UserCard extends HTMLElement {
+    constructor() {
+        super();
+
+        this.showAge = true;
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(userTemplate.content.cloneNode(true));
+        this.shadowRoot.querySelector('.user-name').innerHTML = this.getAttribute('username');
+        this.shadowRoot.querySelector('.user-age').innerHTML = this.getAttribute('age');
+    }
+    
+    toggleAge() {
+        const age = this.shadowRoot.querySelector('.user-age');
+        const buttonText = this.shadowRoot.querySelector('.age-button');
+        age.style.visibility = this.showAge ? 'hidden' : 'visible';
+        buttonText.innerHTML = this.showAge ? 'Show age' : 'Hide age';
+        this.showAge = !this.showAge;
+    }
+    
+    connectedCallback() {
+        this.shadowRoot.querySelector('.age-button').addEventListener('click', () => this.toggleAge());
+    }
+
+    disconnectedCallback() {
+        this.shadowRoot.querySelector('.age-button').removeEventListener();
+    }
+    
+}
+
+window.customElements.define('user-card', UserCard);
